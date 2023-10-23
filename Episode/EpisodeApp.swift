25c51b9e0debd0a4c1c18bc1b2de330fa8062ba6
Øystein Engell <89580365+OysteinEngell@users.noreply.github.com>
2023-10-23 17:07:55 +0200

    Init

---
+++ Episode/EpisodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodeApp.swift
+//  Episode
+//
+//  Created by Øystein Engell on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EpisodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
